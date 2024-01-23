51956c969af480134b7de413a10ce5013a8569c8
Phuoc Dat <nguyenphuocdat18@gmail.com>
2024-01-23 13:43:17 +0700

    no message

---
+++ mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/mapper/UsersMapper.java
@@ -0,0 +1,15 @@
+package com.allxone.mybatisprojectservice.mapper;
+
+import com.allxone.mybatisprojectservice.model.Users;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+
+@Mapper
+public interface UsersMapper {
+
+    Users findByUserName(@Param("userName") String userName);
+
+    Users findByUserId(@Param("userId") Long userId);
+
+}
---
+++ mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/repository/UserRoleDAO.java
@@ -0,0 +1,31 @@
+package com.allxone.mybatisprojectservice.repository;
+
+import com.allxone.mybatisprojectservice.model.UserRole;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserRoleDAO {
+
+    private final SqlSession sqlSession;
+
+    public UserRoleDAO(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public UserRole selectUserByUserId(Long userId) {
+        return this.sqlSession.selectOne("selectUserByUserId", userId);
+    }
+
+    public void updateUserRole(Long userId) {
+        UserRole role = new UserRole(userId);
+        this.sqlSession.update("updateUserRole", role);
+    }
+
+    public void createUserRole(Long userId, Long roleId) {
+        UserRole role = new UserRole(userId, roleId);
+        this.sqlSession.insert("createUserRole", role);
+    }
+
+}
