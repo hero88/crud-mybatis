d88a2a855c93ca8fc7f74b847574fdc5d2e38b04
Âu Thiên Phước <126746510+thienphuoc261@users.noreply.github.com>
2024-01-24 08:00:22 +0700

    finish security for project

--- mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/mapper/UsersMapper.java
+++ mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/mapper/UsersMapper.java
@@ -3,13 +3,18 @@ package com.allxone.mybatisprojectservice.mapper;
 import com.allxone.mybatisprojectservice.model.Users;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 
 @Mapper
+@Repository
 public interface UsersMapper {
-
-    Users findByUserName(@Param("userName") String userName);
 
     Users findByUserId(@Param("userId") Long userId);
 
+    Users findByEmail(String email);
+
+    void insertUser(Users user);
+
+    Users findByEmailAndPassword(String email, String password);
 }
--- mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/model/Users.java
+++ mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/model/Users.java
@@ -1,24 +1,34 @@
 package com.allxone.mybatisprojectservice.model;
 
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
-public class Users implements Serializable {
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users implements UserDetails {
 
-    private static final long serialVersionUID = 1L;
-
-    private Long userId;
-
-    private String userName;
+    private Long id;
 
     private String password;
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
 
     private String gender;
 
     private String address;
 
-    private Integer age;
+    private Byte age;
 
     private Boolean isActive;
 
@@ -26,88 +36,40 @@ public class Users implements Serializable {
 
     private String phoneNumber;
 
+    private Role role;
 
-    public Users(Long userId) {
-        this.userId = userId;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
+    @Override
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public String getEmail() {
+    @Override
+    public String getUsername() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
