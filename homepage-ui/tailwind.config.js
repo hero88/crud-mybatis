c84ccda0af51639580160773a7b2afeea3c97ba0
Dangtrongphuc1902 <phuctrong1902@gmail.com>
2024-01-26 17:48:50 +0700

    update feature issue-1-homepage-ui

--- homepage-ui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  // Path for tailwind find file
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
