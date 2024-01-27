4b606763d4590c1179421cefa5353da04ddecc6b
Long Tran <trangiahoanglong@yahoo.com>
2024-01-27 14:20:49 +0700

    remove unused folder

--- mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/model/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.allxone.mybatisprojectservice.model.enums;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- mybatis-project-service/src/main/java/com/allxone/mybatisprojectservice/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.allxone.mybatisprojectservice.service;
-
-import com.allxone.mybatisprojectservice.model.dto.request.RegisterRequest;
-import com.allxone.mybatisprojectservice.model.dto.response.AuthenticationResponse;
-
-public interface EmailService {
-    void sendConfirmEmail(RegisterRequest user, AuthenticationResponse response);
-}
