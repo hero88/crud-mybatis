0aea396895546deebf744a3a983e27df41155881
TechmoNoway <cuty0406@outlook.com>
2024-01-30 15:27:14 +0700

    update shadcn

---
+++ mybatis-project-frontend/src/layouts/DefaultLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+import axios from "axios";
+
+const DefaultLayout = () => {
+  const callApi = async () => {
+    const { response: data } = await axios.get(
+      "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=100&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap"
+    );
+
+    console.log(data);
+  };
+
+  return (
+    <div onClick={callApi}>
+      <Button>Hello World</Button>
+    </div>
+  );
+};
+
+export default DefaultLayout;
