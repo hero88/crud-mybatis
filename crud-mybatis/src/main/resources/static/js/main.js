21bb0406e919fabe301a3d3a17575a7c2770d6df
Long Tran <trangiahoanglong@yahoo.com>
2024-02-02 16:27:09 +0700

    add log and update import

--- crud-mybatis/src/main/resources/static/js/main.js
+++ crud-mybatis/src/main/resources/static/js/main.js
@@ -3,7 +3,7 @@ import dotenv from 'https://cdn.jsdelivr.net/npm/dotenv@16.4.1/+esm';
 
 dotenv.config();
 
-const gBASE_URL = process.env.HOST_API + '/api';
+const gBASE_URL = import.meta.env.HOST_API + '/api';
 
 //Hàm setCookie
 function setCookie(cname, cvalue, exdays, remember) {
