4a98749eb1507d6f56939122903e1a5e3a42c8c8
Long Tran <trangiahoanglong@yahoo.com>
2024-02-06 13:44:26 +0700

    update pom.xml

--- mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/controller/ActivatedAccountController.java
+++ mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/controller/ActivatedAccountController.java
@@ -1,5 +1,7 @@
 package com.allxone.coinmarket.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
