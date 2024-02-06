5093e28e0705df6b92c748455340ccf21778442e
Long Tran <trangiahoanglong@yahoo.com>
2024-02-06 11:04:56 +0700

    update config

--- frontend/axios/config/config.js
+++ frontend/axios/config/config.js
@@ -2,6 +2,6 @@ import { HOST_URL } from "./env.js";
 import { HOST_PORT } from "./env.js";
 
 export const instance = axios.create({
-    baseURL: `https://${HOST_URL}/api/`,
+    baseURL: `${HOST_URL}/api/`,
     timeout: 1000,
 });
