94e7ce85ae9e8c1bbc0c3a5762cf3183b003f06f
phamsonghiep <phamsonghiepb3k46@gmail.com>
2024-02-15 15:49:11 +0700

    hiep update chat pie and create button login html 15/02/2024

--- frontend/axios/config/config.js
+++ frontend/axios/config/config.js
@@ -2,6 +2,6 @@ import { HOST_URL } from "./env.js";
 import { HOST_PORT } from "./env.js";
 
 export const instance = axios.create({
-    baseURL: `${HOST_URL}/api/`,
+    baseURL: `http://${HOST_URL}:${HOST_PORT}/api/`,
     timeout: 1000,
 });
