e5a8f8b7150a6f756f67d2222a9d3bcde3ffce95
parzival1405 <80945839+parzival1405@users.noreply.github.com>
2024-02-15 09:28:33 +0700

    refresh code

--- mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/config/web/ApplicationConfig.java
+++ mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/config/web/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package com.allxone.mybatisprojectbackend.config.web;
 
 import com.allxone.mybatisprojectbackend.mapper.UserMapper;
-import com.allxone.mybatisprojectbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/controller/CoinController.java
+++ mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/controller/CoinController.java
@@ -1,9 +1,10 @@
 package com.allxone.mybatisprojectbackend.controller;
 
+import com.allxone.mybatisprojectbackend.common.dto.CommonResponse;
 import com.allxone.mybatisprojectbackend.model.Coin;
 import com.allxone.mybatisprojectbackend.service.CoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,87 +14,68 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/coin")
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN','USER')")
 public class CoinController {
 
     private final CoinService coinService;
 
     @GetMapping("")
-    public ResponseEntity<?> getAllCoins(){
-
-        HashMap<String, Object> result = new HashMap<String, Object>();
-
-        try{
+    public CommonResponse<List<Coin>> getAllCoins() {
+        try {
             List<Coin> data = coinService.getAllCoins();
-            result.put("success", true);
-            result.put("message", "");
-            result.put("data", data);
-        }catch (Exception e){
-            result.put("success", false);
-            result.put("message", "");
-            result.put("data", null);
+            return CommonResponse.success(data);
+        } catch (Exception e) {
+            return CommonResponse.error(null);
         }
+    }
 
-        return ResponseEntity.ok(result);
-    };
+    ;
 
     @PostMapping("")
-    public ResponseEntity<?> saveCoin(@RequestBody Coin coin){
+    @PreAuthorize("hasAuthority('admin:create')")
+    public CommonResponse<Coin> saveCoin(@RequestBody Coin coin) {
 
-        HashMap<String, Object> result = new HashMap<String, Object>();
-
-        try{
+        try {
             Coin data = coinService.saveCoin(coin);
-            result.put("success", true);
-            result.put("message", "Saved successfully");
-            result.put("data", data);
-        }catch (Exception e){
-            result.put("success", false);
-            result.put("message", "Failed to save");
-            result.put("data", null);
+            return CommonResponse.success(data);
+        } catch (Exception e) {
+            return CommonResponse.error(null);
         }
+    }
 
-        return ResponseEntity.ok(result);
-    };
+    ;
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteCoinById(@PathVariable Long id){
+    @PreAuthorize("hasAuthority('admin:delete')")
+    public CommonResponse<Long> deleteCoinById(@PathVariable Long id) {
 
-        HashMap<String, Object> result = new HashMap<String, Object>();
-
-        try{
+        try {
             coinService.deleteCoinById(id);
-            result.put("success", true);
-            result.put("message", "Deleted successfully");
-        }catch (Exception e){
-            result.put("success", false);
-            result.put("message", "Failed to delete");
+            return CommonResponse.success(id);
+        } catch (Exception e) {
+            return CommonResponse.error(null);
         }
+    }
 
-        return ResponseEntity.ok(result);
-    };
+    ;
 
     @PutMapping("")
-    public ResponseEntity<?> updateCoin(@RequestBody Coin coin){
+    @PreAuthorize("hasAnyAuthority('admin:update','user:update')")
+    public CommonResponse<Coin> updateCoin(@RequestBody Coin coin) {
 
-        HashMap<String, Object> result = new HashMap<String, Object>();
-        Coin data = coinService.updateCoin(coin);
-        try{
-
-            result.put("success", true);
-            result.put("message", "Updated successfully");
-            result.put("data", data);
-        }catch (Exception e){
-            result.put("success", false);
-            result.put("message", "Failed to update");
-            result.put("data", null);
+        try {
+            Coin data = coinService.updateCoin(coin);
+            return CommonResponse.success(data);
+        } catch (Exception e) {
+            return CommonResponse.error(null);
         }
 
-        return ResponseEntity.ok(result);
-    };
+    }
+
+    ;
 }
