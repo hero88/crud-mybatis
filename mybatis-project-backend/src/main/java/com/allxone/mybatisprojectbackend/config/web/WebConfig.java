d1d1d14f2b1535806d2f2589c3de4d3c547992f1
parzival1405 <80945839+parzival1405@users.noreply.github.com>
2024-02-18 23:06:21 +0700

    add method patch

--- mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/config/web/WebConfig.java
+++ mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/config/web/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                 .allowedHeaders("*");;
     }
 }
