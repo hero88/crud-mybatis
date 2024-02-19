7fe4a2b86130b05466b1361332b9532b7e8ee114
parzival1405 <80945839+parzival1405@users.noreply.github.com>
2024-02-19 11:22:25 +0700

    update api coin chart

--- mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/service/CryptoService.java
+++ mybatis-project-backend/src/main/java/com/allxone/mybatisprojectbackend/service/CryptoService.java
@@ -3,11 +3,12 @@ package com.allxone.mybatisprojectbackend.service;
 
 import com.allxone.mybatisprojectbackend.dto.request.CoinDetailRequest;
 import com.allxone.mybatisprojectbackend.dto.request.CryptoCurrencyRequest;
+import com.allxone.mybatisprojectbackend.dto.response.CoinChartDataResponse;
 import com.allxone.mybatisprojectbackend.dto.response.CoinDetailResponse;
 import com.allxone.mybatisprojectbackend.dto.response.CryptoCurrencyDataResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface CryptoService {
     CryptoCurrencyDataResponse getCoinMarketCapData(CryptoCurrencyRequest request);
-    CoinDetailResponse getCoinMarketCapDetailData(CoinDetailRequest request);
+    CoinChartDataResponse getCoinMarketCapDetailData(CoinDetailRequest request);
 }
