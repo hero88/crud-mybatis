f7d396bf2db98bc684bad556723e459686a5306a
vandu1808 <vandu18082003nv@gmail.com>
2024-02-23 15:11:03 +0700

    vandu/issue-86

---
+++ mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/dto/EmployeesDto.java
@@ -0,0 +1,52 @@
+package com.allxone.coinmarket.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.validation.constraints.NegativeOrZero;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeesDto {
+	private Long id;
+
+	private Long userId;
+
+	private String firstName;
+
+	private String lastName;
+
+	private Date birthday;
+
+	private String gender;
+
+	private String contactNumber;
+
+	private String email;
+
+	private Integer departmentId;
+
+	private String position;
+
+	private Date hireDate;
+
+	private Date terminationDate;
+
+	private Date createdAt;
+
+	private Date updatedAt;
+	
+    private BigDecimal taxRate;
+
+    private Boolean taxExemption;
+    
+    private Integer taxInformationId;
+
+}
--- mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/mapper/DepartmentsMapper.java
+++ mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/mapper/DepartmentsMapper.java
@@ -3,8 +3,11 @@ package com.allxone.coinmarket.mapper;
 import com.allxone.coinmarket.model.Departments;
 import com.allxone.coinmarket.model.DepartmentsExample;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface DepartmentsMapper {
 
 	/**
