25a8d436e6a8b850ddeb7a79da3703c82b989d8b
LilBao <nnvb1311@gmail.com>
2024-03-04 14:12:59 +0700

    filter payroll

--- mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/service/PayrollService.java
+++ mybatis-spring-boot-coinmarket/src/main/java/com/allxone/coinmarket/service/PayrollService.java
@@ -1,6 +1,7 @@
 package com.allxone.coinmarket.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.allxone.coinmarket.dto.response.PayrollDTO;
 
@@ -9,5 +10,7 @@ public interface PayrollService {
     List<PayrollDTO> getAllPayroll(Integer month);
 
     List<PayrollDTO> getAllPayrollByFirstName(Integer month, String firstname);
+
+    List<Object> getNetSalary(List<Integer> listId,Integer monthFrom, Integer monthTo,Integer yearFrom,Integer yearTo);
 
 }
